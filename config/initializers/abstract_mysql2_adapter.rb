663b2eb34f6df3548dedca5bc2fdbbd024d6bced
Ankit Singh <ankit@velvetcase.com>
2017-04-09 19:28:32 +0530

    env change for adapter mysql

--- config/initializers/abstract_mysql2_adapter.rb
+++ config/initializers/abstract_mysql2_adapter.rb
@@ -1,5 +1,7 @@
-require 'active_record/connection_adapters/mysql2_adapter'
+if Rails.env.development?
+  require 'active_record/connection_adapters/mysql2_adapter'
 
-class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+  class ActiveRecord::ConnectionAdapters::Mysql2Adapter
+    NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
+  end
 end
